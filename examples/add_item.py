14d52eca7548b15d525aac95d0b899ece5fd1425
Evgeniy Petrov <blinunleius@gmail.com>
2012-05-27 14:46:04 +0400

    Update to use requests library
    This allows pyzabbix to be both python2 and python3 compitable. I've also tried to by pep8 complaint

--- examples/add_item.py
+++ examples/add_item.py
@@ -2,36 +2,28 @@
 Looks up a host based on its name, and then adds an item to it
 """
 
-from getpass import getpass
 from pyzabbix import ZabbixAPI
 
 # The hostname at which the Zabbix web interface is available
 ZABBIX_SERVER = 'https://zabbix.example.com'
 
-# Enter credentials for the Zabbix Web Frontend
-username = raw_input('Username: ')
-password = getpass()
+zapi = ZabbixAPI(ZABBIX_SERVER)
+zapi.set_http_auth('http_username', 'http_password')
 
-# Connect to the Zabbix web frontend (using the same credentials for HTTPAUTH)
-zapi = ZabbixAPI(ZABBIX_SERVER, username, password)
+# Login to the Zabbix API
+zapi.login('api_username', 'api_password')
 
-# Login to the Zabbix web frontend / API
-zapi.login(username, password)
-
-host_name = raw_input('Add item to what host: ')
+host_name = 'example.com'
 
 hosts = zapi.host.get(filter={"host": host_name})
 if hosts:
     host_id = hosts[0]["hostid"]
-    print "Found host id {0}".format(host_id)
+    print("Found host id {0}".format(host_id))
 
-    zapi.item.create(hostid=host_id,
-                     description='Used disk space on $1 in %',
-                     key_='vfs.fs.size[/,pused]',
-                     )
+    zapi.item.create(
+        hostid=host_id,
+        description='Used disk space on $1 in %',
+        key_='vfs.fs.size[/,pused]',
+    )
 else:
-    print "No hosts found"
-
-
-
-
+    print("No hosts found")
