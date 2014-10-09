5c95e6d9646f91add097be7f772aa9996660f7d6
neothematrix <neothematrix@users.noreply.github.com>
2014-10-10 01:21:19 +0200

    Updated add_item example for Zabbix 2.x

--- examples/add_item.py
+++ examples/add_item.py
@@ -2,7 +2,8 @@
 Looks up a host based on its name, and then adds an item to it
 """
 
-from pyzabbix import ZabbixAPI
+from pyzabbix import ZabbixAPI, ZabbixAPIException
+import sys
 
 # The hostname at which the Zabbix web interface is available
 ZABBIX_SERVER = 'https://zabbix.example.com'
@@ -10,19 +11,28 @@ ZABBIX_SERVER = 'https://zabbix.example.com'
 zapi = ZabbixAPI(ZABBIX_SERVER)
 
 # Login to the Zabbix API
-zapi.login('api_username', 'api_password')
+zapi.login('Admin', 'zabbix')
 
 host_name = 'example.com'
 
-hosts = zapi.host.get(filter={"host": host_name})
+hosts = zapi.host.get(filter={"host": host_name},selectInterfaces=["interfaceid"])
 if hosts:
     host_id = hosts[0]["hostid"]
     print("Found host id {0}".format(host_id))
 
-    zapi.item.create(
-        hostid=host_id,
-        description='Used disk space on $1 in %',
-        key_='vfs.fs.size[/,pused]',
-    )
+    try:
+        item=zapi.item.create(
+            hostid=host_id,
+            name='Used disk space on $1 in %',
+            key_='vfs.fs.size[/,pused]',
+            type=0,
+            value_type=3,
+            interfaceid=hosts[0]["interfaces"][0]["interfaceid"],
+            delay=30
+        )
+    except ZabbixAPIException as e:
+        print(e)
+        sys.exit()
+    print("Added item with itemid {0} to host: {1}".format(item["itemids"][0],host_name))
 else:
     print("No hosts found")
