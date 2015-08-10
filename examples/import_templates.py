922648562e28be6f23628face3b445c82a88e54d
greenmoon55 <greenmoon55@gmail.com>
2015-08-10 12:05:53 +0800

    Add import XML templates example

---
+++ examples/import_templates.py
@@ -0,0 +1,79 @@
+"""
+Import Zabbix XML templates
+"""
+
+from pyzabbix import ZabbixAPI, ZabbixAPIException
+import glob
+
+# The hostname at which the Zabbix web interface is available
+ZABBIX_SERVER = 'https://zabbix.example.com'
+
+zapi = ZabbixAPI(ZABBIX_SERVER)
+
+# Login to the Zabbix API
+zapi.login("admin", "zabbix")
+
+rules = {
+    'applications': {
+        'createMissing': 'true',
+        'updateExisting': 'true'
+    },
+    'discoveryRules': {
+        'createMissing': 'true',
+        'updateExisting': 'true'
+    },
+    'graphs': {
+        'createMissing': 'true',
+        'updateExisting': 'true'
+    },
+    'groups': {
+        'createMissing': 'true'
+    },
+    'hosts': {
+        'createMissing': 'true',
+        'updateExisting': 'true'
+    },
+    'images': {
+        'createMissing': 'true',
+        'updateExisting': 'true'
+    },
+    'items': {
+        'createMissing': 'true',
+        'updateExisting': 'true'
+    },
+    'maps': {
+        'createMissing': 'true',
+        'updateExisting': 'true'
+    },
+    'screens': {
+        'createMissing': 'true',
+        'updateExisting': 'true'
+    },
+    'templateLinkage': {
+        'createMissing': 'true',
+        'updateExisting': 'true'
+    },
+    'templates': {
+        'createMissing': 'true',
+        'updateExisting': 'true'
+    },
+    'templateScreens': {
+        'createMissing': 'true',
+        'updateExisting': 'true'
+    },
+    'triggers': {
+        'createMissing': 'true',
+        'updateExisting': 'true'
+    },
+}
+
+path = './templates/*.xml'
+files = glob.glob(path)
+
+for file in files:
+    with open(file, 'r') as f:
+        template = f.read()
+        try:
+            zapi.confimport('xml', template, rules)
+        except ZabbixAPIException as e:
+            print e
