486e40d58870a14ffc17abc7862f3f66a9feac0f
neothematrix <neothematrix@users.noreply.github.com>
2015-09-12 16:27:35 +0200

    renamed and fixed wrong "trend_data.py" into "history_data.py"

---
+++ examples/history_data.py
@@ -0,0 +1,44 @@
+"""
+Retrieves history data for a given numeric (either int or float) item_id
+"""
+
+from pyzabbix import ZabbixAPI
+from datetime import datetime
+import time
+
+# The hostname at which the Zabbix web interface is available
+ZABBIX_SERVER = 'http://localhost/zabbix'
+
+zapi = ZabbixAPI(ZABBIX_SERVER)
+
+# Login to the Zabbix API
+zapi.login('Admin', 'zabbix')
+
+item_id = 'item_id'
+
+# Create a time range
+time_till = time.mktime(datetime.now().timetuple())
+time_from = time_till - 60 * 60 * 4  # 4 hours
+
+# Query item's history (integer) data
+history = zapi.history.get(itemids=[item_id],
+    time_from=time_from,
+    time_till=time_till,
+    output='extend',
+    limit='5000',
+)
+
+# If nothing was found, try getting it from history (float) data
+if not len(history):
+    history = zapi.history.get(itemids=[item_id],
+        time_from=time_from,
+        time_till=time_till,
+        output='extend',
+        limit='5000',
+        history=0,
+    )
+
+# Print out each datapoint
+for point in history:
+    print("{0}: {1}".format(datetime.fromtimestamp(int(point['clock']))
+    .strftime("%x %X"), point['value']))
