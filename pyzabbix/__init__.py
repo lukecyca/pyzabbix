5b2ac780f772a80d951be92be6edf2d1eae20f76
Gabriele <neothematrix@users.noreply.github.com>
2015-12-11 14:33:26 +0100

    again fixes python3.2 while maintaining python 2.x utf8 compatibility

--- pyzabbix/__init__.py
+++ pyzabbix/__init__.py
@@ -126,7 +126,7 @@ class ZabbixAPI(object):
         if 'error' in response_json:  # some exception
             if 'data' not in response_json['error']: # some errors don't contain 'data': workaround for ZBX-9340
                 response_json['error']['data'] = "No data"
-            msg = u"Error {code}: {message}, {data}".format(
+            msg = "Error {code}: {message}, {data}".format(
                 code=response_json['error']['code'],
                 message=response_json['error']['message'],
                 data=response_json['error']['data']
