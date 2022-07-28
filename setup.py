5cb747a2c723fc6d144e6a69b47f3873629bb093
jo <ljonas@riseup.net>
2022-07-28 15:22:45 +0200

    chore: release 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as fh:
 
 setup(
     name="pyzabbix",
-    version="1.0.0",
+    version="1.1.0",
     description="Zabbix API Python interface",
     long_description=long_description,
     long_description_content_type="text/markdown",
